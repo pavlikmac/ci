fbefb6c007b59913c19d46d3a7a04f6590122928
BirdZhang <0312birdzhang@gmail.com>
2021-02-06 15:12:24 +0800

    fix build for droidmedia and audioflingerglue

--- build-rpm.sh
+++ build-rpm.sh
@@ -21,30 +21,12 @@ sb2 -t $VENDOR-$DEVICE-$PORT_ARCH -m sdk-install -R chmod 777 /boot
 sdk-assistant list
 
 cd $ANDROID_ROOT
-rpm/dhd/helpers/build_packages.sh --droid-hal
+rpm/dhd/helpers/build_packages.sh -d
 
-DROIDMEDIA_VERSION=$(git --git-dir external/droidmedia/.git describe --tags | sed \
--r "s/\-/\+/g")
-rpm/dhd/helpers/pack_source_droidmedia-localbuild.sh $DROIDMEDIA_VERSION
-mkdir -p hybris/mw/droidmedia-localbuild/rpm
-cp rpm/dhd/helpers/droidmedia-localbuild.spec \
-hybris/mw/droidmedia-localbuild/rpm/droidmedia.spec
-sed -ie "s/0.0.0/$DROIDMEDIA_VERSION/" \
-hybris/mw/droidmedia-localbuild/rpm/droidmedia.spec
-mv hybris/mw/droidmedia-$DROIDMEDIA_VERSION.tgz hybris/mw/droidmedia-localbuild
-rpm/dhd/helpers/build_packages.sh --build=hybris/mw/droidmedia-localbuild
 
-AUDIOFLINGERGLUE_VERSION=$(git --git-dir external/audioflingerglue/.git describe --tags | sed \
--r "s/\-/\+/g")
-rpm/dhd/helpers/pack_source_audioflingerglue-localbuild.sh $AUDIOFLINGERGLUE_VERSION
-mkdir -p hybris/mw/audioflingerglue-localbuild/rpm
-cp rpm/dhd/helpers/audioflingerglue-localbuild.spec \
-hybris/mw/audioflingerglue-localbuild/rpm/audioflingerglue.spec
-sed -ie "s/0.0.0/$AUDIOFLINGERGLUE_VERSION/" \
-hybris/mw/audioflingerglue-localbuild/rpm/audioflingerglue.spec
-mv hybris/mw/audioflingerglue-$AUDIOFLINGERGLUE_VERSION.tgz hybris/mw/audioflingerglue-localbuild
-rpm/dhd/helpers/build_packages.sh --build=hybris/mw/audioflingerglue-localbuild
-
-rpm/dhd/helpers/build_packages.sh --droid-hal
+# we do not build gst-droid and gmp-droid, skip them
+sed -i 's/buildmw -u\ \"https\:\/\/github.com\/sailfishos\/gst-droid.git\"/echo skip/g' rpm/dhd/helpers/build_packages.sh 
+sed -i 's/buildmw -u\ \"https\:\/\/github.com\/sailfishos\/gmp-droid.git\"/echo skip/g' rpm/dhd/helpers/build_packages.sh 
+rpm/dhd/helpers/build_packages.sh -g
 
 cat /home/nemo/work/hadk_14.1/droid-hal-$DEVICE.log
